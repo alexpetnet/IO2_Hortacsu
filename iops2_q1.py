273f899b81797030b4b39d340d2038a5e5a4e364
trhierons <trhierons@gmail.com>
2021-02-20 19:15:38 -0600

    Only 1.3.4 and 1.3.5 left for Q1

--- iops2_q1.py
+++ iops2_q1.py
@@ -1,9 +1,12 @@
 # Setup
 import numpy as np
 import scipy as sp
+import scipy.stats as sps
 from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
 np.random.seed(1234)
 
+# 1.3.2 ------------------------------------------------------------------------
 # Equilibrium conditions for a BNE
 def bne(p,α,δ,x):
     f1 = p[0] - np.exp(α*x - δ*p[1])/(1+np.exp(α*x - δ*p[1]))
@@ -18,39 +21,66 @@ def solve_bne(α,δ,x):
     sols = np.round(np.apply_along_axis(solve, 1, grid),6)
     return np.unique(sols, axis=0)
 
-# Compute for the given params
-print(solve_bne(1,1,1))
-print(solve_bne(3,6,1))
-print(solve_bne(1,1,2))
-print(solve_bne(3,6,2))
+# Same restricted to the symmetric case
+def sym_bne(p,α,δ,x):
+    return p - np.exp(α*x - δ*p)/(1+np.exp(α*x - δ*p))
 
+def solve_sym_bne(α,δ,x):
+    solve = lambda p :sym_bne(p,α,δ,x)
+    return fsolve(solve,0)[0]
 
+# Tell us the answers
+for z in [(1,1,1),(3,6,1),(1,1,2),(3,6,2)]:
+    print("The equilibrium probabilities for (α,δ,x)=" + str(z) + " are " +\
+    str(solve_bne(*z)))
+
+# 1.3.3 ------------------------------------------------------------------------
 # Generate sample using the symmetric selection rule
-def gen_sym_sample(α,δ,T,S):
-    '''Y1 and Y2 are (T,S) shaped arrays of decisions for each firm in each
-    market, x is an (T,) shaped array of the given variable'''
-    ε = np.random.logistic(loc=0,scale=1,size=(T,S,2))
-    X = 1+np.random.binomial(n=1,p=.5,size=(T,S))
-    sol1 = solve_bne(α,δ,1)[1,0]
-    sol2 =  solve_bne(α,δ,2)[1,0]
-    Y1 = (((X==1)*(α*X-δ*sol1)+(X==2)*(α*X-δ*sol1))<=ε[:,:,0])+0
-    Y2 = (((X==1)*(α*X-δ*sol1)+(X==2)*(α*X-δ*sol1))<=ε[:,:,1])+0
+def gen_sym_sample(α,δ,T):
+    '''Y1 and Y2 are (T,) shaped arrays of decisions for each firm in each
+    market, X is also a (T,) array for the realised Bernoulli variable'''
+    ε = np.random.logistic(loc=0,scale=1,size=(T,2))
+    X = 1+np.random.binomial(n=1,p=.5,size=(T,))
+    p1,p2 = solve_sym_bne(α,δ,1), solve_sym_bne(α,δ,2)
+    Y1 = (((X==1)*(α*X-δ*p1)+(X==2)*(α*X-δ*p2))>=-ε[:,0])+0
+    Y2 = (((X==1)*(α*X-δ*p1)+(X==2)*(α*X-δ*p2))>=-ε[:,1])+0
     return Y1, Y2, X
 
-# Solve bne under symmetry assumption
+# Probabilities across markets in the symmetric case
+def sym_prob(α,δ,X):
+    p1,p2 = solve_sym_bne(α,δ,1), solve_sym_bne(α,δ,2)
+    return p1*(X==1) + p2*(X==2)
 
-# Log likelihood given data and proposed equilibrium probabilities
+# Minus Log likelihood given data and proposed equilibrium probabilities
+def sym_llik(α,δ,Y1,Y2,X,P):
+    F = sps.logistic.cdf(δ*P-α*X)
+    return -np.sum((Y1+Y2)*np.log(1-F) + (2-Y1-Y2)*np.log(F))
 
 # Maximise the log likelihood as the solution to a fixed point problem
 def sym_mle(Y1,Y2,X,α0,δ0):
     ''' α0 and δ0 are the initial candidate parameter values'''
-    return α,δ
+    iter = 0
+    diff = 1
+    θo = np.array([α0,δ0])
+    while (iter < 1000)&(diff>1e-6):
+        P = sym_prob(θo[0],θo[1],X)
+        sll = lambda φ: sym_llik(φ[0],φ[1],Y1,Y2,X,P)
+        res = sp.optimize.minimize(sll,θo)
+        liknew, θn = res.fun, res.x
+        iter+=1
+        diff = np.sum(np.abs(θn-θo))
+        θo=θn
+    return θn
 
 
- Y1,Y2,X = gen_sym_sample(3,6,1000,50)
+# Conduct a Monte Carlo study and save the results as a histogram
+results = np.zeros(50,2)
+
+Y1,Y2,X = gen_sym_sample(3,6,1000)
+sym_mle(Y1,Y2,X,1,1)
 
 
-
+# 1.3.4 ------------------------------------------------------------------------
 # Generate sample using the probabilistic selection rule
 def gen_prob_sample(α,δ,T,S):
     '''Y1 and Y2 are (T,S) shaped arrays of decisions for each firm in each
