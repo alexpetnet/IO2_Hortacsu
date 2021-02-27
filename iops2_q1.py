e0877783ff62c483ffbedcda4a85094817da9413
trhierons <trhierons@gmail.com>
2021-02-26 19:14:38 -0600

    Finished 1.3.4 and 1.3.5

--- iops2_q1.py
+++ iops2_q1.py
@@ -15,6 +15,8 @@ def bne(p,α,δ,x):
 
 # Solving the fixed points for BNE with a variety of start points
 def solve_bne(α,δ,x):
+    ''' note that relative to the pset notation for increasing probabilities in
+    k, the 0th column is player 2 and the 1st column is player 1'''
     bne_p = lambda p: bne(p,α,δ,x)
     solve = lambda x0: fsolve(bne_p,x0)
     grid = np.mgrid[0:1:1e-1, 0:1:1e-1].reshape(2,-1).T
@@ -73,31 +75,169 @@ def sym_mle(Y1,Y2,X,α0,δ0):
     return θn
 
 # Conduct a Monte Carlo study and save the results as a histogram
-results = np.zeros((50,2))
-for i in range(50):
+print("Starting First Monte Carlo")
+S = 50
+results = np.zeros((S,2))
+for i in range(S):
     Y1,Y2,X = gen_sym_sample(3,6,1000)
     θ = sym_mle(Y1,Y2,X,1,1)
     results[i,:]= θ
 
-
 plt.hist(results[:,0], density=True, label="Histogram")
 plt.xlabel("α")
 plt.ylabel("Density")
-plt.savefig("figs/hist_alpha.png")
+plt.savefig("figs/hist_alpha_det.png")
 plt.close()
 
 
 plt.hist(results[:,1], density=True, label="Histogram")
 plt.xlabel("δ")
 plt.ylabel("Density")
-plt.savefig("figs/hist_delta.png")
+plt.savefig("figs/hist_delta_det.png")
 plt.close()
 
 
 
 # 1.3.4 ------------------------------------------------------------------------
+# Equilibrium probabilities
+def eq_probs(K):
+    v = np.array([np.exp((i+1)/2) for i in range(K)])
+    return v/np.sum(v)
+
+# Equilibrium selection mechanism
+def sel_eq(λ):
+    return np.random.choice(np.array(list(range(len(λ)))),p=λ)
+
 # Generate sample using the probabilistic selection rule
-def gen_prob_sample(α,δ,T,S):
+def gen_prob_sample(α,δ,T):
     '''Y1 and Y2 are (T,S) shaped arrays of decisions for each firm in each
-    market, X is a (T,S) shaped array of the given variable'''
+    market, X is a (T,S) shaped array of the given variable
+    Note: relative to pset notation for increasing probabilities,
+    player numbers are flipped.'''
+    ε = np.random.logistic(loc=0,scale=1,size=(T,2))
+    X = 1+np.random.binomial(n=1,p=.5,size=(T,))
+    p1,p2 = solve_bne(α,δ,1), solve_bne(α,δ,2)
+    Y1,Y2 = np.zeros(T),np.zeros(T)
+    for t in range(T):
+        if X[t]==1:
+            p = p1[sel_eq(eq_probs(3)),:]
+        else:
+            p = p2[sel_eq(eq_probs(3)),:]
+        Y1[t] = (α*X[t] - δ*p[1] >= -ε[t,0])+0
+        Y2[t] = (α*X[t] - δ*p[0] >= -ε[t,1])+0
     return Y1, Y2, X
+
+# Return equilibrium probabilities given realised X and conjectured α,δ
+def prob_sel(α,δ):
+    p1 = solve_bne(α,δ,1)
+    p2 = solve_bne(α,δ,2)
+    return p1,p2
+
+# Minus Log likelihood given data, and conjectured probabilities and
+# equilibrium selection
+def prob_llik(α,δ,Y1,Y2,X,p1,p2):
+    ''' P is (K,T,2): number of equilibria x markets x players'''
+    K1,K2 = p1.shape[0],p2.shape[0]
+    λ1,λ2 = eq_probs(K1),eq_probs(K2)
+    T = len(X)
+    # player 1 sums
+    p111 = np.array([λ1[i]*(1- sps.logistic.cdf(δ*p1[i,1]-α*X)) \
+    for i in range(K1)])
+    p112 =np.array([λ2[i]*(1- sps.logistic.cdf(δ*p2[i,1]-α*X)) \
+    for i in range(K2)])
+    p11 = np.sum([X==1]*p111,axis=0) + np.sum([X==2]*p112,axis=0)
+    # player 2 sums
+    p211 = np.array([λ1[i]*(1- sps.logistic.cdf(δ*p1[i,0]-α*X)) \
+    for i in range(K1)])
+    p212 =np.array([λ2[i]*(1- sps.logistic.cdf(δ*p2[i,0]-α*X)) \
+    for i in range(K2)])
+    p21 = np.sum([X==1]*p211,axis=0) + np.sum([X==2]*p212,axis=0)
+    # combine into likelihood
+    ll = Y1*np.log(p11)+ (1-Y1)*np.log(1-p11) + \
+    Y2*np.log(p21)+ (1-Y2)*np.log(1-p21)
+    return -np.sum(ll)
+
+# MLE estimator
+def prob_mle(Y1,Y2,X,α0,δ0):
+    ''' α0 and δ0 are the initial candidate parameter values'''
+    iter = 0
+    diff = 1
+    θo = np.array([α0,δ0])
+    while (iter < 100)&(diff>1e-6):
+        p1, p2 = prob_sel(θo[0],θo[1])
+        sll = lambda φ: prob_llik(φ[0],φ[1],Y1,Y2,X,p1,p2)
+        res = sp.optimize.minimize(sll,θo)
+        liknew, θn = res.fun, res.x
+        iter+=1
+        diff = np.sum(np.abs(θn-θo))
+        θo=θn
+    return θn
+
+# Conduct a Monte Carlo study and save the results as a histogram
+print("Starting second Monte Carlo")
+S = 50
+results2 = np.zeros((S,2))
+for i in range(S):
+    print(i)
+    Y1,Y2,X = gen_prob_sample(3,6,1000)
+    θ = prob_mle(Y1,Y2,X,1,2)
+    results2[i,:]= θ
+
+plt.hist(results2[:,0], density=True, label="Histogram")
+plt.xlabel("α")
+plt.ylabel("Density")
+plt.savefig("figs/hist_alpha_prob.png")
+plt.close()
+
+
+plt.hist(results2[:,1], density=True, label="Histogram")
+plt.xlabel("δ")
+plt.ylabel("Density")
+plt.savefig("figs/hist_delta_prob.png")
+plt.close()
+
+# 1.3.6 Extra analysis ---------------------------------------------------------
+
+# Conduct a Monte Carlo study using part 3 algorithm on part 4 data
+print("Starting third Monte Carlo")
+S = 50
+results3 = np.zeros((S,2))
+for i in range(S):
+    Y1,Y2,X = gen_prob_sample(3,6,1000)
+    θ = sym_mle(Y1,Y2,X,1,2)
+    results3[i,:]= θ
+
+plt.hist(results3[:,0], density=True, label="Histogram")
+plt.xlabel("α")
+plt.ylabel("Density")
+plt.savefig("figs/hist_alpha_det_on_prob.png")
+plt.close()
+
+
+plt.hist(results3[:,1], density=True, label="Histogram")
+plt.xlabel("δ")
+plt.ylabel("Density")
+plt.savefig("figs/hist_delta_det_on_prob.png")
+plt.close()
+
+# Conduct a Monte Carlo study using part 4 algorithm on part 3 data
+print("Starting fourth Monte Carlo")
+S = 50
+results4 = np.zeros((S,2))
+for i in range(S):
+    Y1,Y2,X = gen_sym_sample(3,6,1000)
+    θ = prob_mle(Y1,Y2,X,1,1)
+    results4[i,:]= θ
+
+plt.hist(results4[:,0], density=True, label="Histogram")
+plt.xlabel("α")
+plt.ylabel("Density")
+plt.savefig("figs/hist_alpha_prob_on_det.png")
+plt.close()
+
+
+plt.hist(results4[:,1], density=True, label="Histogram")
+plt.xlabel("δ")
+plt.ylabel("Density")
+plt.savefig("figs/hist_delta_prob_on_det.png")
+plt.close()
