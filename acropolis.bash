9597dccf5bdc3470f23b125a9a67e8c399aa9760
alexpetnet <45228318+alexpetnet@users.noreply.github.com>
2021-02-26 23:41:56 -0600

    finished q2

---
+++ acropolis.bash
@@ -0,0 +1,3 @@
+ssh -i ~/athensx -L localhost:8889:localhost:8984 alexpetrov@acropolis.uchicago.edu
+
+python3 iops2_q2.py > output_iops2_q2.txt
--- iops2_q2.py
+++ iops2_q2.py
@@ -5,8 +5,6 @@ from scipy.optimize import minimize
 
 data = pd.read_csv('data/psetTwo.csv')
 
-data
-data[data.milage > 180]
 K = 10
 β = .999
 
@@ -15,8 +13,6 @@ M = data.iloc[:, 0]
 d = np.array([int(M[i] < M[i - 1]) for i in range(1, len(M))])
 d_p = np.insert(d, 0, 0) # need to subset those rows that are after changing engine
 d = np.append(d, 0) # this is the replacement variable
-d_p[4999]
-d[26]
 
 cap = max(M)
 X = np.arange(cap, step = cap / K)
@@ -30,23 +26,23 @@ lc = np.array([[int(M[j] > X[len(X) - 1])] for j in range(len(M))])
 mat = np.concatenate((mat, lc), axis = 1)
 mat
 
-mat[d_p == 1].shape
+# the transition matrix for d = 1
 T1 = np.sum(mat[d_p == 1], axis = 0) / np.sum(mat[d_p == 1])
 T1 = np.tile(T1, (len(X), 1))
-T1.shape
 
+# the transition matrix for d = 0
 T0 = np.zeros((len(X), len(X))) #initialising
 for j in range(len(X)):
 
     tr = np.array([(r[j] == 1) for r in mat])
     tr[d == 1] = False
-    tr[4999] = False
+    tr[len(M) - 1] = False
     tr = np.roll(tr, 1)
 
     T0[j] = np.sum(mat[tr], axis = 0) / np.sum(mat[tr])
 
 T0
-# pick rows and exclude the last before replacement
+
 
 def u0(x, θ1, θ2):
     return - θ1 * x - θ2 * ((x / 100) ** 2)
@@ -57,7 +53,6 @@ def u1(x, θ3):
 
 V_0 = np.zeros(K)
 
-# need transition matrices
 def FP(V_0, θ, T0, T1, ϵ):
 
     θ1 = θ[0]
@@ -89,8 +84,39 @@ def FP(V_0, θ, T0, T1, ϵ):
 
     return V_new
 
+# to addreess the nan issue, increase the precision of EV
+def FP_longdouble(V_0, θ, T0, T1, ϵ):
 
-#np.exp(-np.float128(750))
+    θ1 = θ[0]
+    θ2 = θ[1]
+    θ3 = θ[2]
+
+    V_old = V_0
+
+    U0 = np.array([u0(x, θ1, θ2) for x in X])
+    C0 = β * V_old
+
+    U1 = np.array([u1(x, θ3) for x in X])
+    C1 = β * np.repeat(V_old[0], K)
+
+    inner_sum = np.exp(np.longdouble(U0 + C0)) + np.exp(np.longdouble(U1 + C1))
+    V_new = T0 @ np.log(inner_sum)
+
+    while max(np.abs(V_new - V_old)) > ϵ:
+        V_old = V_new
+
+        C0 = β * V_old
+
+        C1 = β * np.repeat(V_old[0], K)
+
+        inner_sum = np.exp(np.longdouble(U0 + C0)) + np.exp(np.longdouble(U1 + C1))
+        #print(inner_sum)
+        V_new = T0 @ np.log(inner_sum)
+        #print(V_new)
+
+    return V_new
+
+
 v = FP(V_0, np.array([.01, .01, .01]), T0, T1, 0.00001)
 v
 
@@ -135,6 +161,7 @@ def newton(V0, θ, ϵ):
     Jac = J(θ, V_old, T0)
 
     V_new = V_old - np.linalg.inv(np.eye(K) - Jac) @ (V_old - ΓV)
+    #V_new = V_old - np.linalg.solve(np.eye(K) - Jac, V_old - ΓV)
 
     while max(np.abs(V_new - V_old)) > ϵ:
         V_old = V_new
@@ -149,10 +176,11 @@ def newton(V0, θ, ϵ):
         Jac = J(θ, V_old, T0)
 
         V_new = V_old - np.linalg.inv(np.eye(K) - Jac) @ (V_old - ΓV)
+        #V_new = V_old - np.linalg.solve(np.eye(K) - Jac, V_old - ΓV)
 
     return V_new
 
-newton(V_0, np.array([.01, .01, .01]), np.power(0.1, 10))
+newton(V_0, np.array([.1, .1, .1]), np.power(0.1, 10))
 
 
 def poly(V0, θ, ϵ_fp, ϵ_n):
@@ -163,7 +191,7 @@ def poly(V0, θ, ϵ_fp, ϵ_n):
 
     return EV_n
 
-poly(V_0, np.array([.01, .01, .01]), np.power(0.1, 10), np.power(0.1, 16))
+poly(V_0, np.array([.1, .1, .1]), np.power(0.1, 10), np.power(0.1, 16))
 
 
 def l(θ, m, d, V, T0, T1):
@@ -176,8 +204,6 @@ def l(θ, m, d, V, T0, T1):
     lc = m >= X[len(X) - 1]
     ind = np.append(ind, lc)
 
-    #l0 = np.exp(u0(m, θ1, θ2) + β * T0[ind] @ V)
-    #l1 = np.exp(u1(m, θ3) + β * T1[0] @ V)
     l0 = np.exp(u0(m, θ1, θ2) + β * V[ind])
     l1 = np.exp(u1(m, θ3) + β * V[0])
 
@@ -185,13 +211,6 @@ def l(θ, m, d, V, T0, T1):
 
     return (1 - d) * np.log(p0) + d * np.log(1 - p0)
 
-    #if d == 0:
-        #print(np.longdouble(l0 / sum))
-    #    return np.log(np.longdouble(l0 / sum))
-
-    #if d == 1:
-    #    print(np.longdouble(l1 / sum))
-    #    return np.log(np.longdouble(l1 / sum))
 
 
 def dl(θ, m, d, V, dp):
@@ -212,33 +231,29 @@ def dl(θ, m, d, V, dp):
     return (d / (1 - p0)) * (- dp[ind, :]) + ((1 - d) / p0) * dp[ind, :]
 
 
+dl(np.array([.01, .01, .01]), 1, 1, v, Jac)
 
 
+def L(θ, M, D, ld):
 
-dl(np.array([.01, .01, .01]), 1, 1, v, J, T0)
-
-
-def L(θ, M, D):
-
+    # this is to choose whether to use increased precision or not
+    if ld == True:
+        V = FP_longdouble(V_0, θ, T0, T1, np.power(0.1, 10))
+    else:
+        V = FP(V_0, θ, T0, T1, np.power(0.1, 10))
     #V = FP(V_0, θ, T0, T1, 0.000001)
-    V = poly(V_0, θ, np.power(0.1, 10), np.power(0.1, 16))
+    #V = poly(V_0, θ, np.power(0.1, 10), np.power(0.1, 16))
+    #V = FP_longdouble(V_0, θ, T0, T1, np.power(0.1, 10))
 
     lik = np.array([l(θ, M[i], D[i], V, T0, T1) for i in range(len(M))])
 
-    #for i in range(len(M)):
-    #    if lik[i] == 0:
-    #        print("0")
 
-    #product = 1
-    #for i in range(len(M)):
-    #    product = product * lik[i]
+    Lik = - np.sum(lik)
+    print(Lik)
+    return Lik
 
-    return np.sum(lik)
-    #return float(product)
 
-l(np.array([.1, 1, 1]), 10, 0, v, T0, T1)
-
-L(np.array([1, 1, 1]), M, d)
+L(np.array([1, 1, 1]), M, d, False)
 
 
 def dL(θ, M, D):
@@ -247,8 +262,9 @@ def dL(θ, M, D):
     θ2 = θ[1]
     θ3 = θ[2]
 
-    #V = FP(V_0, θ, T0, T1, 0.000001)
-    V = poly(V_0, θ, np.power(0.1, 10), np.power(0.1, 16))
+    V = FP(V_0, θ, T0, T1, 0.000001)
+    #V = poly(V_0, θ, np.power(0.1, 10), np.power(0.1, 16))
+    #V = FP_longdouble(V_0, θ, T0, T1, np.power(0.1, 10))
 
     U0 = np.array([u0(x, θ1, θ2) for x in X])
     U1 = np.array([u1(x, θ3) for x in X])
@@ -274,30 +290,25 @@ def dL(θ, M, D):
 
     D = np.array([dl(θ, M[i], D[i], V, dp) for i in range(len(M))])
 
-    return np.sum(D, axis = 0)[0]
+    return - np.sum(D, axis = 0)[0]
 
 out = dL(np.array([.01, .01, .01]), M, d)
 out
 
 
-st = 0.01 * np.zeros(3)
-opt = minimize(L, x0 = st, args = (M, d), method = 'BFGS', jac = dL)
-opt.x
+st = np.zeros(3)
+opt = minimize(L, x0 = st, args = (M, d, False), method = 'BFGS', jac = dL)
+print("The estimated parameters are " + str(opt.x))
+
+# optimizing without gradient but with increased precision
+# doesn't really help
+opt_pr = minimize(L, x0 = st, args = (M, d, True), method = 'BFGS', options = {'disp' : True})
+opt_pr.x
+#L(opt_pr.x, M, d)
 
 
-θ_chase = [-0.0009379236102099465,
-  0.009966453941326345,
-  0.0100117408828595]
- θ_chase
- L(θ_chase, M, d)
-dL(θ_chase, M, d)
 
-θ_sam = [-0.001146669162127232,
-  0.009888876180159625,
-  0.010113097294607182]
-L(θ_sam, M, d)
 
-L(st, M, d)
 
 
 ##### JAX #####
@@ -311,6 +322,8 @@ M_jax = jnp.array(M, dtype = float)
 M_jax
 d_jax = jnp.array(d, dtype = float)
 d_jax
+
+V_0_jax = jnp.zeros(K)
 
 def FP_jax(V_0, θ, T0, T1, ϵ):
 
@@ -354,9 +367,9 @@ def poly_jax(V0, θ, ϵ_fp, ϵ_n):
 
 def L_jax(θ):
 
-    #V = FP(V_0, θ, T0, T1, 0.000001)
+    V = FP_jax(V_0_jax, θ, T0, T1, 0.000001)
     #V = poly_jax(V_0, θ, np.power(0.1, 10), np.power(0.1, 16))
-    V = newton(V_0, θ, np.power(0.1, 10))
+    #V = newton(V_0, θ, np.power(0.1, 10))
 
     lik = np.array([l(θ, M_jax[i], D_jax[i], V, T0, T1) for i in range(len(M_jax))])
 
@@ -368,7 +381,7 @@ def L_jax(θ):
     #for i in range(len(M)):
     #    product = product * lik[i]
 
-    return np.sum(lik)
+    return - np.sum(lik)
     #return float(product)
 
 dL_jax = grad(L_jax)
