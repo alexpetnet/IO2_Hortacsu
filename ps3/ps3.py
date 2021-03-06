8ef5ee9b8dc6b7f49623e0988dcb8e69b283028a
trhierons <trhierons@gmail.com>
2021-03-06 17:48:51 -0600

    Setup

---
+++ ps3/ps3.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import os
+# 1 Data --- -------------------------------------------------------------------
+df = pd.read_csv("ps3/data/ps3.csv", skiprows = [0])
+
+# Table 1:
+
+# Table 2:
+
+
+
+# 2 Introductory Questions -----------------------------------------------------
+
+# 3 Structural Analysis --------------------------------------------------------
